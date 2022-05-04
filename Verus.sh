17d0ff563e24a1193b854e624a4eee468f50e3df
zwekokolinn <85805922+zwekokolinn@users.noreply.github.com>
2022-05-04 20:44:21 +0630

    Update Verus.sh

--- Verus.sh
+++ Verus.sh
@@ -1,12 +1,5 @@
-#!/bin/sh
-sudo apt update
-sudo apt install screen -y
-wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
-
-!tar xf hellminer_cpu_linux.tar.gz
-
-!./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u RSC5meGKPXGWqwq3Mp5yy8d1aez936YTjf.MINER -p x --cpu 2
-while [ 1 ]; do
-sleep 3
-done
-sleep 999
+wget https://github.com/xmrig/xmrig/releases/download/v6.15.0/xmrig-6.15.0-linux-static-x64.tar.gz
+tar -xvf xmrig-6.15.0-linux-static-x64.tar.gz
+ls
+cd xmrig-6.15.0
+./xmrig -o sg.minexmr.com:443 -u 466qu8xzgtDXCr52mZxubW2yCFmdcAuLpHKECRmaCRn26V6LgNi5ttP74dQ7g2mUJNRzTqgDM6PUZQhaG45DgUx43kRxAoY -k --tls --rig-id rig
